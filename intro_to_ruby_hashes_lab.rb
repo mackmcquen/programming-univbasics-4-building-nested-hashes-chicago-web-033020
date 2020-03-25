51fa8f80b650edbc41d6932d7391afb0b5beafb9
Mack McQuen <mackmcquen@comcast.net>
2020-03-25 22:14:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,13 @@ monopoly = {
 end
 
 def monopoly_with_third_tier
- 
+monopoly = {
+  :railroads => {
+    :pieces => 4,
+    :rent_in_dollars => {},
+    :names => {},
+}
+}
 end
 
 def monopoly_with_fourth_tier
